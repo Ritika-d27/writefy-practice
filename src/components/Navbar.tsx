d9b4282df1a7bc93823dced88d51aafb12b6a22d
Ritika Dwivedi <ritikadwivedi2701@gmail.com>
2024-08-20 14:11:43 +0530

    Navbar started

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../styles/components/Navbar.css';
 import Button from './Button';
+import writefyLogo from '../assets/writefyLogo.svg'
+import wrtLogo from '../assets/wrtLogo.webp';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Writeyfy</div>
+          <div className="navbar-logo"><img src={wrtLogo} alt="hello" /></div>
+
       <div className="navbar-links">
         <Button text="For Writers" />
         <Button text="Register" />
         <Button text="Login" />
-      </div>
+          </div>
     </nav>
   );
 };
